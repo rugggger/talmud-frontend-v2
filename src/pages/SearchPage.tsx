63dd25a775f0d9cf1653fb57bebfbeab89fe5365
hadar hubara <hadarhubara10@gmail.com>
2024-07-15 19:47:52 +0300

    search page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,12 @@
-import { FC, useEffect } from 'react';
+import { FC, Fragment, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { searchText } from '../store/actions/searchActions';
-import { useSearchParams } from 'react-router-dom';
-import { Box, Card } from '@mui/material';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Box, Card, Typography } from '@mui/material';
 import { ISearchResult } from '../store/reducers/searchReducer';
+import { base64ToJson } from '../inc/base64ToJson';
+import { hebrewMap } from '../inc/utils';
+import PageService from '../services/pageService';
 
 interface IProps {}
 
@@ -12,6 +15,8 @@ const SearchPage: FC<IProps> = () => {
   const searchResults = useAppSelector((state: any) => state?.search?.searchResults) as ISearchResult[];
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query') || '';
+  const [allTractates, setAllTractates] = useState<any[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (query) {
@@ -19,11 +24,60 @@ const SearchPage: FC<IProps> = () => {
     }
   }, [dispatch, query]);
 
+  const queryObject = base64ToJson(query);
+
+  useEffect(() => {
+    PageService.getAllTractates().then((tractates) => {
+      setAllTractates(tractates);
+    });
+  }, []);
+
   return (
-    <Box display="flex" flexDirection="column" gap={2} p={5}>
-      {searchResults.map((result, index) => (
-        <Card key={index}>{result?.mainLine}</Card>
-      ))}
+    <Box display="flex" gap={{ md: 2, xs: 5 }} p={5} flexDirection="column" alignItems="center">
+      {searchResults.map((result, index) => {
+        const [tractate, chapter, mishna] = result?.guid.split('_');
+        return (
+          <Card
+            key={index}
+            sx={{
+              width: '80%',
+              p: 4,
+              mb: 4,
+              cursor: 'pointer',
+              borderRadius: 3,
+              boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
+              transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+              background: 'white',
+              '&:hover': {
+                transform: 'scale(1.02)',
+                boxShadow: '0px 6px 25px rgba(0, 0, 0, 0.15)',
+              },
+            }}
+            onClick={() => {
+              navigate(`/talmud/${tractate}/${chapter}/${mishna}`);
+            }}>
+            <Typography sx={{ fontSize: 18, mb: 2 }}>
+              {allTractates.find((item) => item.id === tractate)?.title_heb}, {hebrewMap.get(chapter)},{' '}
+              {hebrewMap.get(mishna)}
+            </Typography>
+            <Typography>
+              {result?.mainLine.split(' ').map((word, index) => (
+                <Fragment key={index}>
+                  <span
+                    key={index}
+                    style={{
+                      backgroundColor: word.includes(queryObject.text) ? 'yellow' : 'transparent',
+                      fontSize: 17,
+                      lineHeight: 1.8,
+                    }}>
+                    {word}
+                  </span>{' '}
+                </Fragment>
+              ))}
+            </Typography>
+          </Card>
+        );
+      })}
     </Box>
   );
 };
